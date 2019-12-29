893168e0d4548432cfc9afae85a799ff803d6382
Ajay Kumar Bharti <ajay.ajaykumar.kumar494@gmail.com>
2019-12-29 13:41:00 +0530

    Add all my files

---
+++ app/src/main/java/in/eeehub/websearch/MainActivity.java
@@ -0,0 +1,25 @@
+package in.eeehub.websearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.wvID);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://www.eeehub.in");
+
+
+
+
+    }
+}
